9ea12d322b39374bad3145d046eeba00534e6d20
Alex <ugwuanyicornelius53@gmail.com>
2022-12-08 20:07:50 -0800

    converting roman numeral to interger

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    integer = 0
+    roman_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+                 'D': 500, 'M': 1000}
+    prev_numeral = ''
+
+    if not roman_string or type(roman_string) is not str:
+        return integer
+
+    for numeral in roman_string:
+        if numeral not in roman_int:
+            return integer
+        if (prev_numeral and roman_int[prev_numeral] < roman_int[numeral]):
+            integer += roman_int[numeral] - roman_int[prev_numeral] * 2
+        else:
+            integer += roman_int[numeral]
+        prev_numeral = numeral
+    return int(integer)
