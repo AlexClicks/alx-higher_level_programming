8cf22eff6dcdf583e1e09f31b03b58af1ed23c7d
Alex <ugwuanyicornelius53@gmail.com>
2022-12-21 00:46:39 -0800

    Class with a private instance

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Creates an initialized Class"""
+
+class Square:
+    """Square class with __init__"""
+    def __init__(self, size=0):
+        """initialize an instance of the Square class and validate the size arg
+        Args:
+            size (int): size of the square
+        """
+        if type(size) == int:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
