c600b79847d21e1c9f6b000d8bb058481f805285
Alex <ugwuanyicornelius53@gmail.com>
2023-01-07 00:58:13 -0800

    added some pyhton TDD

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,7 +3,9 @@
 
 def add_integer(a, b=98):
     """Return the addition of the intergers; a and b.
+
     Float arguments are typecasted to ints before addition is performed.
+
     Raises:
         TypeError: If either of a or b is a non-integer and non-float.
     """
