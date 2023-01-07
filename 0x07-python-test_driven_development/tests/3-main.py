3e9b35b4425949aa5332b1c2d42f7c541ce3aa64
Alex <ugwuanyicornelius53@gmail.com>
2023-01-07 12:32:44 +0100

    Added python TDD

---
+++ 0x07-python-test_driven_development/tests/3-main.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+say_my_name = __import__('3-say_my_name').say_my_name
+
+say_my_name("John", "Smith")
+say_my_name("Walter", "White")
+say_my_name("Bob")
+try:
+    say_my_name(12, "White")
+except Exception as e:
+    print(e)
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+3-main.py [unix] (00:59 01/01/1970)                                                                                                                                                      0,1 All
+-- INSERT --
+
