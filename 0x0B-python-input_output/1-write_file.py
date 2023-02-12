04c7d05578afe7039abd7b81d69142aae1b22bda
Alex <ugwuanyicornelius53@gmail.com>
2023-02-12 14:32:55 +0100

    writing to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def write_file(filename="fle.txt", text="This is the text"):
+def write_file(filename="", text=""):
     with open(file.txt, 'w', encoding='utf-8') as f:
         f.write(text)
     return len(text)
