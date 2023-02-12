df236a99ad2dffdbd913fb159c2dfa4364561beb
Alex <ugwuanyicornelius53@gmail.com>
2023-02-12 13:51:09 +0100

    reading from a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,6 @@
+#!/usr/bin/python3
 def read_file(Text.txt):
-    with open(Text, 'r', encoding='utf8') as f:
+    with open(Text.txt, 'r', encoding='utf8') as f:
         for line in f:
             print(line, end='')
 
